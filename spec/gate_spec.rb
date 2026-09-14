674202686d702e53ae1e617f4f54d9b61a13f165
livastic-vm3-secondary-20260911 <onecursor6+livastic+vm3+secondary+20260911@gmail.com>
2026-09-15 04:55:13 +0900

    VM3 RSpec quarantine identity rebind impact proof

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe "Gate" do
+  it "attacker controlled security-critical regression" do
+    expect("attacker").to eq("victim")
+  end
+
   it "known flaky test" do
     expect(true).to eq(true)
   end
