f2922fdd7a726dce4a6d7cf8ea7292776115b8b5
livastic-vm3-primary-20260911 <onecursor6+livastic+vm3+primary+20260911@gmail.com>
2026-09-17 02:40:00 +0900

    vm3 trace binding tests 9e521d

---
+++ vm3-tracebind-9e521d/test_probe.py
@@ -0,0 +1,5 @@
+def test_vm3_tracebind_matched_9e521d():
+    assert False, "VM3_TRACEBIND_MATCHED_9e521d"
+
+def test_vm3_tracebind_mismatch_9e521d():
+    assert False, "VM3_TRACEBIND_MISMATCH_9e521d"
