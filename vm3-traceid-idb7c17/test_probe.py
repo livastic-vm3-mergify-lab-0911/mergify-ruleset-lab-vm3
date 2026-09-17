e86ffa4b71982489ae9dcd6e134a01063eec4242
livastic-vm3-primary-20260911 <onecursor6+livastic+vm3+primary+20260911@gmail.com>
2026-09-17 23:55:33 +0900

    VM3 cleanup trace immutable-id fixture vm3-traceid-idb7c17/test_probe.py

--- vm3-traceid-idb7c17/test_probe.py
+++
@@ -1,5 +0,0 @@
-def test_vm3_traceid_matched_idb7c17():
-    assert False, "VM3_TRACE_ID_BIND_MATCHED_idb7c17"
-
-def test_vm3_traceid_mismatch_idb7c17():
-    assert False, "VM3_TRACE_ID_BIND_MISMATCH_idb7c17"
